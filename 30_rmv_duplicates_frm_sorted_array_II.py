5f2492e3e0e64c743551962f04334f70b5847be2
BluePhoenix01 <hi2a@ymail.com>
2025-08-10 23:59:08 -0700

    Q31 Length of longest substring without reapting characters

--- 30_rmv_duplicates_frm_sorted_array_II.py
+++ 30_rmv_duplicates_frm_sorted_array_II.py
@@ -1,4 +1,4 @@
-def removeDuplicates(nums):
+def removeDuplicates2(nums):
     j = 0
     count = 0
     for i in range(1, len(nums)):
@@ -7,9 +7,25 @@ def removeDuplicates(nums):
             nums[j] = nums[i]
             count = 0
         count += 1
-        
-    return nums[:j+1], j+1
 
-print(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))  
-print(removeDuplicates([1, 1, 1, 2, 2, 3]))
-print(removeDuplicates([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+    return nums[: j + 1], j + 1
+
+
+def removeDuplicates2Better(nums):
+    if not nums:
+        return 0
+    j = 2
+    for i in range(2, len(nums)):
+        if nums[i] != nums[j - 2]:
+            nums[j] = nums[i]
+            j += 1
+    return nums[:j], j
+
+
+# print(removeDuplicates2([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
+# print(removeDuplicates2([1, 1, 1, 2, 2, 3]))
+# print(removeDuplicates2([1, 1, 2, 2, 2, 3]))
+
+print(removeDuplicates2Better([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
+print(removeDuplicates2Better([1, 1, 1, 2, 2, 3]))
+print(removeDuplicates2Better([1, 1, 2, 2, 2, 3]))
