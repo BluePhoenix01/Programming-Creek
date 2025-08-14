de83a82826421efe111a04fe3d37fe8d496585e3
BluePhoenix01 <hi2a@ymail.com>
2025-08-14 16:12:10 -0700

    Q42 Largest Number

---
+++ 43_largest_num.py
@@ -0,0 +1,20 @@
+def largestNum(nums):
+    if not nums:
+        return "0"
+    nums = [str(num) for num in nums]
+    nums.sort(reverse=True)
+    right = 1
+ 
+    while right < len(nums):
+        if int(nums[right - 1] + nums[right]) < int(nums[right] + nums[right - 1]):
+            nums[right - 1], nums[right] = nums[right], nums[right - 1]
+        right += 1    
+    max_num = ''.join(nums)
+    return max_num
+def largestNumLambda(nums):
+    return (lambda nums: ''.join(sorted(map(str, nums), key=lambda x: x*10, reverse=True)))(nums)
+
+print(largestNum([3, 30, 34, 5, 9]))  # Output: "9534330"
+
+
+
