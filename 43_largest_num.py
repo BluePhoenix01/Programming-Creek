ccb74ea2970e44f34c42414060979e21dd4e969a
BluePhoenix01 <hi2a@ymail.com>
2025-08-14 21:03:54 -0700

    Revert "Q43* Largest Number O(n^2)"
    This reverts commit 5a2b18f31e4e7b7b42011e8155e864df2026c335.

--- 43_largest_num.py
+++ 43_largest_num.py
@@ -6,13 +6,15 @@ def largestNum(nums):
     right = 1
  
     while right < len(nums):
-        while int(nums[right - 1] + nums[right]) < int(nums[right] + nums[right - 1]):
+        if int(nums[right - 1] + nums[right]) < int(nums[right] + nums[right - 1]):
             nums[right - 1], nums[right] = nums[right], nums[right - 1]
-            right -= 1
         right += 1    
     max_num = ''.join(nums)
     return max_num
 def largestNumLambda(nums):
     return (lambda nums: ''.join(sorted(map(str, nums), key=lambda x: x*10, reverse=True)))(nums)
 
-print(largestNum([30, 3, 30, 34, 5, 9]))  # Output: "9534330"
+print(largestNum([3, 30, 34, 5, 9]))  # Output: "9534330"
+
+
+
