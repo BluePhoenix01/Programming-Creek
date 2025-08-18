0f8cb95ece11c25d680eee6be9d8eb91ef4d7ab6
BluePhoenix01 <hi2a@ymail.com>
2025-08-18 14:12:10 -0700

    Q57 Spiral Matrix

---
+++ 57_Spiral_Matrix.py
@@ -0,0 +1,35 @@
+def spiralMatrix(matrix):
+    if not matrix or not matrix[0]:
+        return []
+
+    result = []
+    top, bottom = 0, len(matrix) - 1
+    left, right = 0, len(matrix[0]) - 1
+
+    while top <= bottom and left <= right:
+        # Traverse from left to right
+        for i in range(left, right + 1):
+            result.append(matrix[top][i])
+        top += 1
+
+        # Traverse from top to bottom
+        for i in range(top, bottom + 1):
+            result.append(matrix[i][right])
+        right -= 1
+
+        # Traverse from right to left
+        for i in range(right, left - 1, -1):
+            if top <= bottom:
+                result.append(matrix[bottom][i])
+        bottom -= 1
+
+        # Traverse from bottom to top
+        for i in range(bottom, top - 1, -1):
+            if left <= right:
+                result.append(matrix[i][left])
+        left += 1
+
+    return result
+
+print(spiralMatrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))  # Output: [1, 2, 3, 6, 9, 8, 7, 4, 5]
+print(spiralMatrix([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]))  # Output: [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]
